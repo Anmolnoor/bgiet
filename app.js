aee97643942ec8e6af51a59e0b2ad64e1b5fc984
Anmol Noor <anmolnoor59@gmail.com>
2020-09-09 00:50:36 +0530

    add dirname to public

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const { concat } = require("lodash");
 
 // ======================= app ======================
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(__dirname +"public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
